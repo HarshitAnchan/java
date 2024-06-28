d2fb593147e01129a25c5673adc7e348f06e9a69
harshitanchan <harshitanchan647@gmail.com>
2024-06-28 23:18:18 +0530

    java

---
+++ src/TypeCasting.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+////        float num = input.nextFloat();
+//
+//        System.out.println();
+
+//
+//        int num =(int)(67.56f);
+//        System.out.println(num);
+
+        byte a =40;
+        byte b = 50;
+        byte c = 89;
+        int d = (a * b) / c;
+        System.out.println(d);
+
+
+
+    }
+}
