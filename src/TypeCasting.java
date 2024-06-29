6537c19ca722e4614e28581796948ee40bc17e22
Harshit Anchan <88927163+HarshitAnchan@users.noreply.github.com>
2024-06-29 20:20:19 +0530

    Add files via upload

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -10,12 +10,24 @@ public class TypeCasting {
 //
 //        int num =(int)(67.56f);
 //        System.out.println(num);
+//
+//        byte a =40;
+//        byte b = 50;
+//        byte c = 89;
+//        int d = (a * b) / c;
+//        System.out.println(d);
 
-        byte a =40;
-        byte b = 50;
-        byte c = 89;
-        int d = (a * b) / c;
-        System.out.println(d);
+
+        byte b =42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = 0.1234;
+        double result = (f*b) + (i/c) - (d-s);
+
+        System.out.println((f*b) +" "+ (i/c) +" "+ (d-s));
+        System.out.println(result);
 
 
 
